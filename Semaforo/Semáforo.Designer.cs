ff7e34f2f7268511949b20309e8d1b2b34f61d1d
Adam Samuel <20198316@itla.edu.do>
2021-12-04 20:50:02 -0400

    first commit

---
+++ Semaforo/Semáforo.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Semáforo
+{
+    partial class Semáforo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Semáforo));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnencender = new System.Windows.Forms.Button();
+            this.btnapagar = new System.Windows.Forms.Button();
+            this.btndetener = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnencender
+            // 
+            this.btnencender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(186)))), ((int)(((byte)(60)))));
+            this.btnencender.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnencender.Font = new System.Drawing.Font("Century Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnencender.ForeColor = System.Drawing.Color.White;
+            this.btnencender.Location = new System.Drawing.Point(328, 128);
+            this.btnencender.Name = "btnencender";
+            this.btnencender.Size = new System.Drawing.Size(130, 40);
+            this.btnencender.TabIndex = 0;
+            this.btnencender.Text = "Encender";
+            this.btnencender.UseVisualStyleBackColor = false;
+            this.btnencender.Click += new System.EventHandler(this.btnencender_Click);
+            // 
+            // btnapagar
+            // 
+            this.btnapagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.btnapagar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnapagar.Font = new System.Drawing.Font("Century Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnapagar.ForeColor = System.Drawing.Color.White;
+            this.btnapagar.Location = new System.Drawing.Point(328, 286);
+            this.btnapagar.Name = "btnapagar";
+            this.btnapagar.Size = new System.Drawing.Size(130, 40);
+            this.btnapagar.TabIndex = 1;
+            this.btnapagar.Text = "Apagar";
+            this.btnapagar.UseVisualStyleBackColor = false;
+            this.btnapagar.Click += new System.EventHandler(this.btnapagar_Click);
+            // 
+            // btndetener
+            // 
+            this.btndetener.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(203)))), ((int)(((byte)(23)))));
+            this.btndetener.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btndetener.Font = new System.Drawing.Font("Century Gothic", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndetener.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btndetener.Location = new System.Drawing.Point(328, 207);
+            this.btndetener.Name = "btndetener";
+            this.btndetener.Size = new System.Drawing.Size(130, 40);
+            this.btndetener.TabIndex = 2;
+            this.btndetener.Text = "Detener";
+            this.btndetener.UseVisualStyleBackColor = false;
+            this.btndetener.Click += new System.EventHandler(this.btndetener_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Semáforo Vehicular";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(522, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Semáforo Peatonal";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(531, 71);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(212, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Semáforo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(28)))), ((int)(((byte)(75)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(770, 398);
+            this.Controls.Add(this.btndetener);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnapagar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnencender);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Semáforo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simulador de Semáforo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnencender;
+        private System.Windows.Forms.Button btnapagar;
+        private System.Windows.Forms.Button btndetener;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Semaforo/Semáforo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semáforo
+{
+    public partial class Semáforo : Form
+    {
+        int caso = 0;
+
+        public Semáforo()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Semaforo.Properties.Resources.verde;
+            pictureBox2.Image = Semaforo.Properties.Resources.peatonalverde;
+            btnapagar.Enabled = false;
+            btndetener.Enabled = false;
+        }
+
+        private void btnencender_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnencender.Enabled = false;
+            btndetener.Enabled = true;
+            btnapagar.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (caso)
+            {
+                case 0:
+                    pictureBox1.Image = Semaforo.Properties.Resources.verde;
+                    timer1.Interval = 7000;
+                    pictureBox2.Image = Semaforo.Properties.Resources.peatonalrojo;
+                    caso = 1;
+                    break;
+                case 1:
+                    pictureBox1.Image = Semaforo.Properties.Resources.amarillo;
+                    timer1.Interval = 5000;
+                    caso = 2;
+                    break;
+                case 2:
+                    pictureBox1.Image = Semaforo.Properties.Resources.rojo;
+                    timer1.Interval = 10000;
+                    pictureBox2.Image = Semaforo.Properties.Resources.peatonalverde;
+                    caso = 0;
+                    break;
+            }
+        }
+
+        private void btndetener_Click(object sender, EventArgs e)
+        {
+            if (btndetener.Text == "Detener")
+            {
+                timer1.Enabled = false;
+                btndetener.Text = "Reanudar";
+            }
+            else
+              if (btndetener.Text == "Detener")
+            {
+                timer1.Enabled = true;
+                btndetener.Text = "Detener";
+            }
+        }
+
+        private void btnapagar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btndetener.Enabled = false;
+            btnapagar.Enabled = false;
+            btnencender.Enabled = true;
+        }
+    }
+}
